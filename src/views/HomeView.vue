b900efee2d22b89ba48c586c9b87ba4405fb9089
MashinaSSokom <skltan@mail.ru>
2022-10-17 20:28:42 +0300

    add render search component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Search from "@/components/SearchUsers";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Search
   }
-
 }
 </script>
