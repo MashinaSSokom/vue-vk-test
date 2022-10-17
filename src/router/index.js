eefa7d347cae2117d340d5efb24778d60d06a6fd
MashinaSSokom <skltan@mail.ru>
2022-10-17 19:14:39 +0300

    add persisted-vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: ()=>import('../views/HomeView')
+  },
+  {
+    path: '/profile/:id',
+    name: 'profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProfileView.vue')
   },
   {
     path: '/about',
