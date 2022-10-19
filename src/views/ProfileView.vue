d3b5c02874d811883da6eb8f1da717578e998c08
MashinaSSokom <skltan@mail.ru>
2022-10-19 17:01:40 +0300

    add Profile user info

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="profile">
     <router-link to="/">Назад</router-link>
+    <template v-if="this.getProfile">
+      <div class="user-info">
+        <div class="user-info__avatar">
+          <img :src="this.getProfile.photo_200_orig" alt="">
+        </div>
+        <div class="user-info__description">
+          <p>Имя: {{ `${this.getProfile.last_name} ${this.getProfile.first_name}` }}</p>
+          <p>Возраст:
+            {{ computeBDate(this.getProfile.bdate) }}</p>
+          <p>Пол: {{ this.getProfile.sex === 2 ? 'Мужчина' : 'Женщина' }}</p>
+          <p>Количество друзей: {{ `${this.getProfile.counters.friends}` }}</p>
+          <a :href="`https://vk.com/id${this.getProfile.id}`">Профиль в ВК</a>
+        </div>
+        <div class="user-info__friends">
+
+        </div>
+      </div>
+    </template>
     <template v-if="this.getFetchedWall">
       <div class="posts">
         <!--TODO: Сделать подгрузку следующих 10 постов-->
@@ -35,26 +53,60 @@ export default {
   beforeMount() {
     const userId = this.$route.params.id
     this.fetchUserWall({userId: userId})
+    this.fetchUserFriends({userId: userId})
+    this.fetchProfile({userId: userId})
   },
   computed: {
-    ...mapGetters(['getFetchedWall']),
+    ...mapGetters(['getFetchedWall', 'getFetchedUserFriends', 'getProfile']),
   },
   methods: {
-    ...mapActions(['fetchUserWall'])
+    ...mapActions(['fetchUserWall', 'fetchUserFriends', 'fetchProfile']),
+    computeBDate(dateString) {
+      if (dateString) {
+        let d = dateString.split('.')
+        if (d.length === 3) {
+          const bDate = new Date(d[2], d[1]-1, d[0])
+          return Math.floor((Date.now() - bDate)/(1000*60*60*24*365))
+        }
+      }
+      return 'Нет информации'
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.posts {
-  width: 75%;
+.profile {
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: start;
 
-  .post {
-    width: 25%;
-    padding: 5px;
-    border: 1px solid grey;
+  .user-info {
+    display: flex;
+    margin-top: 20px;
+
+    .user-info__avatar {
+      margin-right: 15px;
+    }
+
+    .user-info__description {
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+    }
+  }
+
+  .posts {
+    width: 75%;
+    display: flex;
+    flex-wrap: wrap;
+
+    .post {
+      width: 25%;
+      padding: 5px;
+      border: 1px solid grey;
+    }
   }
 }
+
 </style>
